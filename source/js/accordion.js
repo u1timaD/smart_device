bbfe6d44f46a180c036cb35d9467160260849b32
Dmitry <free-doom46@mail.ru>
2022-12-15 19:11:40 +0300

    first commit

---
+++ source/js/accordion.js
@@ -0,0 +1,45 @@
+let accordion = document.querySelectorAll('.accordion');
+let footerNav = document.querySelector('.footer__navigation');
+let mobileWidthMediaQuery = window.matchMedia('(max-widht: 768px');
+
+
+
+accordion.forEach((el) => {
+
+  if (window.screen.width <= 768) {
+    footerNav.classList.add('accordion--close');
+    el.nextElementSibling.classList.add('nav--close');
+  } else {
+    footerNav.classList.remove('accordion--close');
+    el.nextElementSibling.classList.remove('nav--close');
+  }
+
+  el.addEventListener('click', () => {
+    if (footerNav.classList.contains('accordion--close')) {
+      el.nextElementSibling.classList.toggle('nav--close');
+      el.classList.toggle('accordion-is-open');
+    }
+  });
+});
+
+(function () {
+
+  matchMedia('only screen and (min-width:768px)').addEventListener('change', () => {
+
+    if (footerNav.classList.contains('accordion--close')) {
+      footerNav.classList.remove('accordion--close');
+    } else {
+      footerNav.classList.add('accordion--close');
+    }
+
+    accordion.forEach((el) => {
+
+      if (footerNav.classList.contains('accordion--close')) {
+        el.nextElementSibling.classList.add('nav--close');
+      } else {
+        el.nextElementSibling.classList.remove('nav--close');
+      }
+    });
+  });
+})();
+
